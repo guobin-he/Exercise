812fe01a49062cc6b79b70ecc0106e3b5118b115
Guobin He <ghe0077@gmail.com>
2015-09-15 15:39:40 +0900

    Adding log4j2 code - not completed yet

--- src/com/ghe/exercise/HelloWorld.java
+++ src/com/ghe/exercise/HelloWorld.java
@@ -1,7 +1,12 @@
 package com.ghe.exercise;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class HelloWorld {
-    public static void main(String[] args) {
+	private static final Logger logger = LogManager.getLogger("HelloWorld");
+	public static void main(String[] args) {
         System.out.println("Hello World me");
+        logger.info("Hello, World!");
     }
 }
